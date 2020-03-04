c15c85730b28f252b7a4515343e0239fd5cd96cf
taku1508 <60663610+taku1508@users.noreply.github.com>
2020-03-04 12:20:32 +0900

    depuroi updete

--- Gemfile
+++ Gemfile
@@ -39,6 +39,17 @@ gem 'bootsnap', '>= 1.1.0', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'pry-rails'
+  gem 'capistrano'
+  gem 'capistrano-rbenv'
+  gem 'capistrano-bundler'
+  gem 'capistrano-rails'
+  gem 'capistrano3-unicorn'
+  gem 'capistrano-rails-console'
+end
+
+group :production do
+  gem 'unicorn'
 end
 
 group :development do
---
+++ config/deploy.rb
@@ -0,0 +1,36 @@
+# config valid only for current version of Capistrano
+# capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
+lock '3.12.0'
+
+# Capistranoのログの表示に利用する
+set :application, 'freeemarket_70f'
+
+# どのリポジトリからアプリをpullするかを指定する
+set :repo_url,  'git@github.com:taku1508/freeemarket_70f.git'
+
+# バージョンが変わっても共通で参照するディレクトリを指定
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
+
+# どの公開鍵を利用してデプロイするか
+set :ssh_options, auth_methods: ['publickey'],
+                  keys: ['~/.ssh/chatspace.pem'] 
+
+# プロセス番号を記載したファイルの場所
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+# Unicornの設定ファイルの場所
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    # unicornを完全にキルしてから再起動する
+    invoke 'unicorn:stop'
+    invoke 'unicorn:start'
+  end
+  after :finishing, 'deploy:cleanup'
+end
