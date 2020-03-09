7f8566d1db04d0eb51eece5def37d81bed62188d
taku1508 <60663610+taku1508@users.noreply.github.com>
2020-03-09 19:38:56 +0900

    画像編集

--- db/schema.rb
+++ db/schema.rb
@@ -77,13 +77,6 @@ ActiveRecord::Schema.define(version: 2020_03_09_052226) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "nickname", null: false
-    t.string "first_name", null: false
-    t.string "second_name", null: false
-    t.string "hurigana_first", null: false
-    t.string "hurigana_second", null: false
-    t.integer "birthday", null: false
-    t.integer "phone_number"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
