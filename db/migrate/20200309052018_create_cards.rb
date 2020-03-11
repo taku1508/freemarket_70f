52861b210dd0f8268f07aedab56bffb370291961
cof-ryo <haokaryo@gmail.com>
2020-03-11 12:06:51 +0900

    データベースの変更（外部キー参照ができていなかったため）

--- db/migrate/20200309052018_create_cards.rb
+++ db/migrate/20200309052018_create_cards.rb
@@ -1,10 +1,9 @@
 class CreateCards < ActiveRecord::Migration[5.2]
   def change
     create_table :cards do |t|
-      t.integer :number, null: false
-      t.integer :limit, null: false
-      t.integer :cord, null: false
-      t.integer :user_id, null: false, foreign_key: true
+      t.references :user ,foreign_key: true, null: false
+      t.string :customer_id, null: false
+      t.string :card_id, null: false
       t.timestamps
     end
   end
--- db/migrate/20200309052059_create_images.rb
+++ db/migrate/20200309052059_create_images.rb
@@ -2,7 +2,7 @@ class CreateImages < ActiveRecord::Migration[5.2]
   def change
     create_table :images do |t|
       t.string :image, null: false
-      t.integer :item_id, null: false, foreign_key: true
+      t.references :item, null: false, foreign_key: true
       t.timestamps
     end
   end
