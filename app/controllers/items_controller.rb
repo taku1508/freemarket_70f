72f1de029a73a73c7b38bd1d7a00cb709c98f687
imo-tastu <tasuki.soccer46@gmail.com>
2020-03-17 11:59:25 +0900

    [WIP]商品詳細ページ途中

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,8 @@
 class ItemsController < ApplicationController
-  before_action :get_payjp_info, only: [:confirm]
 
   def index
-    # @items = Item.all
+    @item = Item.all
     @items = Item.order("created_at DESC")
-    @images = Image.all
   end
   
   def new
@@ -19,7 +17,7 @@ class ItemsController < ApplicationController
     # @category = Category.all
     # @item.category = category_id
     # @item.brand_id = @item.brand
-
+    # @item.category_id = 1
     binding.pry
     if @item.save
       redirect_to item_path(params[:id]), notice: 'アイテムを作成しました。'
@@ -29,11 +27,8 @@ class ItemsController < ApplicationController
   end
 
   def  done
-
     @product_purchaser= Product.find(params[:id])
     @product_purchaser.update( user_id: current_user.id)
-
-
   end
 
   def show
@@ -50,12 +45,6 @@ class ItemsController < ApplicationController
   end
 
   def confirm
-    @card = current_user.cards.first
-    @user = current_user
-    if @card.present?
-      customer = Payjp::Customer.retrieve(@card.customer_id)
-      @default_card_information = customer.cards.retrieve(@card.card_id)
-    end
     @item = Item.find(params[:id])
     if @item.soldout == 1
       redirect_to item_path(@item.id)
@@ -65,18 +54,7 @@ class ItemsController < ApplicationController
   private
 
   def items_params
-
-    params.require(:item).permit(:nickname,:description,:status,:shipping_charges,:area,:days,:price,images_attributes: [:image,:id]).merge(user_id: current_user.id, category_id: 1)
-  end
-
-  # payjpをしようするためのメソッド
-  def get_payjp_info 
-    if Rails.env == 'development'
-      Payjp.api_key = ENV["PAYJP_ACCESS_KEY"]
-    else
-      Payjp.api_key = Rails.application.credentials.payjp[:PAYJP_ACCESS_KEY]
-    end
-
+    params.require(:item).permit(:nickname,:description,:status,:shipping_charges,:area,:days,:price,images_attributes: [:image, :id]).merge(user_id: current_user.id, category_id: 1)
   end
 
 end
