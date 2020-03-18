5883362df6107d3af491aedcef25b07a1e89d93f
taku1508 <60663610+taku1508@users.noreply.github.com>
2020-03-18 15:09:56 +0900

    edit
    edit

--- config/deploy.rb
+++ config/deploy.rb
@@ -50,3 +50,10 @@ namespace :deploy do
   before :starting, 'deploy:upload'
   after :finishing, 'deploy:cleanup'
 end
+
+set :default_env, {
+  rbenv_root: "/usr/local/rbenv",
+  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+}
