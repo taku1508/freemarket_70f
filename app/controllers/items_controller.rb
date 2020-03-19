e72edd7f8b6ec216868777960c7a9fc949ec7e2e
taku1508 <60663610+taku1508@users.noreply.github.com>
2020-03-19 18:43:02 +0900

    商品一覧表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,9 +8,25 @@ class ItemsController < ApplicationController
   
   def new
     @item = Item.new
-    @item.images.new 
-  end
+    @item.images.new
+    @category_parent_array = ["---"]
+    Category.where(ancestry: nil).each do |parent|
+      @category_parent_array << parent.name
+    end
   
+    def get_category_children
+      #選択された親カテゴリーに紐付く子カテゴリーの配列を取得
+      @category_children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
+    end
+
+    def get_category_grandchildren
+      #選択された子カテゴリーに紐付く孫カテゴリーの配列を取得
+      @category_grandchildren = Category.find("#{params[:child_id]}").children
+    end
+
+  end
+
+
   def create
     @item = Item.new(items_params)
     if @item.save(items_params)
