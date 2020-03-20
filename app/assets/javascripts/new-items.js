e60aecd1cd9a45c2760d5e3e4df4c2762ef3b918
Neru-dum <tanten180@gmail.com>
2020-03-20 15:47:17 +0900

    コンフリクト解消

--- app/assets/javascripts/new-items.js
+++ app/assets/javascripts/new-items.js
@@ -1,65 +1,65 @@
-// $(function(){
-//   //DataTransferオブジェクトで、データを格納する箱を作る
-//   var dataBox = new DataTransfer();
-//   //querySelectorでfile_fieldを取得
-//   var file_field = document.querySelector('input[type=file]')
-//   //fileが選択された時に発火するイベント
-//   $('#img-file').change(function(){
-//     //選択したfileのオブジェクトをpropで取得
-//     var files = $('input[type="file"]').prop('files')[0];
-//     $.each(this.files, function(i, file){
-//       //FileReaderのreadAsDataURLで指定したFileオブジェクトを読み込む
-//       var fileReader = new FileReader();
+$(function(){
+  //DataTransferオブジェクトで、データを格納する箱を作る
+  var dataBox = new DataTransfer();
+  //querySelectorでfile_fieldを取得
+  var file_field = document.querySelector('input[type=file]')
+  //fileが選択された時に発火するイベント
+  $('#img-file').change(function(){
+    //選択したfileのオブジェクトをpropで取得
+    var files = $('input[type="file"]').prop('files')[0];
+    $.each(this.files, function(i, file){
+      //FileReaderのreadAsDataURLで指定したFileオブジェクトを読み込む
+      var fileReader = new FileReader();
 
-//       //DataTransferオブジェクトに対して、fileを追加
-//       dataBox.items.add(file)
-//       //DataTransferオブジェクトに入ったfile一覧をfile_fieldの中に代入
-//       file_field.files = dataBox.files
+      //DataTransferオブジェクトに対して、fileを追加
+      dataBox.items.add(file)
+      //DataTransferオブジェクトに入ったfile一覧をfile_fieldの中に代入
+      file_field.files = dataBox.files
 
-//       var num = $('.item-image').length + 1 + i
-//       fileReader.readAsDataURL(file);
-//        //画像が5枚になったら超えたらドロップボックスを削除する
-//       if (num == 5){
-//         $('#image-box__container').css('display', 'none')   
-//       }
-//       //読み込みが完了すると、srcにfileのURLを格納
-//       fileReader.onloadend = function() {
-//         var src = fileReader.result
-//         // var html= `<div class='item-image' data-image="${file.name}">
-//         //             <div class=' item-image__content'>
-//         //               <div class='item-image__content--icon'>
-//         //                  <img src=${src} width="124" height="123" > 
-//         //               </div>
-//         //             </div>
-//         //             <div class='item-image__operetion'>
-//         //               <div class='item-image__operetion--delete'>削除</div>
-//         //             </div>
-//         //           </div>`
+      var num = $('.item-image').length + 1 + i
+      fileReader.readAsDataURL(file);
+       //画像が5枚になったら超えたらドロップボックスを削除する
+      if (num == 5){
+        $('#image-box__container').css('display', 'none')   
+      }
+      //読み込みが完了すると、srcにfileのURLを格納
+      fileReader.onloadend = function() {
+        var src = fileReader.result
+        // var html= `<div class='item-image' data-image="${file.name}">
+        //             <div class=' item-image__content'>
+        //               <div class='item-image__content--icon'>
+        //                  <img src=${src} width="124" height="123" > 
+        //               </div>
+        //             </div>
+        //             <div class='item-image__operetion'>
+        //               <div class='item-image__operetion--delete'>削除</div>
+        //             </div>
+        //           </div>`
 
-//         //  var html= `<div class='item-num-0'>
-//         //               <div id='img-file' data-image="${file.name}">                                                                   
-//         //                 <img src=${src} width="124" height="123" > 
-//         //                   <div class='item-image__operetion--delete'>削除</div>
-//         //                 </div>
-//         //             </div>`
+        //  var html= `<div class='item-num-0'>
+        //               <div id='img-file' data-image="${file.name}">                                                                   
+        //                 <img src=${src} width="124" height="123" > 
+        //                   <div class='item-image__operetion--delete'>削除</div>
+        //                 </div>
+        //             </div>`
 
-//         var html= `<div class='img-file'>
-//                     <div class='item-image' data-image="${file.name}">
-//                     <div class=' item-image__content'>
-//                       <div class='item-image__content--icon'>
-//                           <img src=${src} width="124" height="123" > 
-//                       </div>
-//                     </div>
-//                     <div class='item-image__operetion'>
-//                       <div class='item-image__operetion--delete'>削除</div>                      
-//                     </div>
-//                   </div>`
-//         //image_box__container要素の前にhtmlを差し込む
-//         $('#image-box__container').before(html);
-//         return html;
-//       };
-//       //image-box__containerのクラスを変更し、CSSでドロップボックスの大きさを変えてやる。
-//       $('#image-box__container').attr('class', `item-num-${num}`)
-//     });
-//   });
-// })
+        var html= `<div class='img-file'>
+                    <div class='item-image' data-image="${file.name}">
+                    <div class=' item-image__content'>
+                      <div class='item-image__content--icon'>
+                          <img src=${src} width="124" height="123" > 
+                      </div>
+                    </div>
+                    <div class='item-image__operetion'>
+                      <div class='item-image__operetion--delete'>削除</div>                      
+                    </div>
+                  </div>`
+        //image_box__container要素の前にhtmlを差し込む
+        $('#image-box__container').before(html);
+        return html;
+      };
+      //image-box__containerのクラスを変更し、CSSでドロップボックスの大きさを変えてやる。
+      $('#image-box__container').attr('class', `item-num-${num}`)
+    });
+  });
+})
