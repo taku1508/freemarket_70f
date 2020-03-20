d6422eb242020ef39c86dba257769c0e9a0f465e
Neru-dum <tanten180@gmail.com>
2020-03-20 15:43:56 +0900

    出品機能差分　差分

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,95 +1,95 @@
-class ItemsController < ApplicationController
-  before_action :set_item, only: [:show,:destroy, :edit, :update]
-  def index
-    @item = Item.all
-    @items = Item.order("created_at DESC").limit(3)
-    @random = Item.order("RAND()").limit(3)
-  end
+# class ItemsController < ApplicationController
+#   before_action :set_item, only: [:show,:destroy, :edit, :update]
+#   def index
+#     @item = Item.all
+#     @items = Item.order("created_at DESC").limit(3)
+#     @random = Item.order("RAND()").limit(3)
+#   end
   
-  def new
-    @item = Item.new
-    @item.images.new
-    # @category_parent_array = ["---"]
-    @category_parent = Category.where(ancestry: nil)
-      # @category_parent_array << parent.name
-    # end
+#   def new
+#     @item = Item.new
+#     @item.images.new
+#     # @category_parent_array = ["---"]
+#     @category_parent = Category.where(ancestry: nil)
+#       # @category_parent_array << parent.name
+#     # end
   
-    def get_category_children
-      #選択された親カテゴリーに紐付く子カテゴリーの配列を取得
-      @category_children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
-    end
+#     def get_category_children
+#       #選択された親カテゴリーに紐付く子カテゴリーの配列を取得
+#       @category_children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
+#     end
 
-    def get_category_grandchildren
-      #選択された子カテゴリーに紐付く孫カテゴリーの配列を取得
-      @category_grandchildren = Category.find("#{params[:child_id]}").children
-    end
+#     def get_category_grandchildren
+#       #選択された子カテゴリーに紐付く孫カテゴリーの配列を取得
+#       @category_grandchildren = Category.find("#{params[:child_id]}").children
+#     end
 
-  end
-
-
-  def create
-    @item = Item.new(items_params)
-    if @item.save(items_params)
-      redirect_to  items_path(@item.id), notice: 'アイテムを出品しました。'
-    else
-      flash.now[:alert] = 'アイテムの出品に失敗しました。'
-      render :index
-    end
-  end
+#   end
 
 
-  def show
-  end
+#   def create
+#     @item = Item.new(items_params)
+#     if @item.save(items_params)
+#       redirect_to  items_path(@item.id), notice: 'アイテムを出品しました。'
+#     else
+#       flash.now[:alert] = 'アイテムの出品に失敗しました。'
+#       render :index
+#     end
+#   end
 
-  def edit
-  end
 
-  def update
-    if @item.update(item_params)
-      redirect_to root_path
-    end
-  end
+#   def show
+#   end
 
-  def destroy
-    @item.destroy
-    redirect_to root_path
-    flash[:alert] = '削除しました。'
-  end
+#   def edit
+#   end
 
-  def confirm
-    if current_user.blank?
-      redirect_to root_path
-      flash[:alert] = 'ログインを行なってください。'
-    else
-      @card = current_user.cards.first
-      if @card.present?
-        customer = Payjp::Customer.retrieve(@card.customer_id)
-        @default_card_information = customer.cards.retrieve(@card.card_id)
-      end
-      @item = Item.find(params[:id])
-      if @item.soldout == 1
-        redirect_to item_path(@item.id)
-      end
-    end
-  end
+#   def update
+#     if @item.update(item_params)
+#       redirect_to root_path
+#     end
+#   end
 
-  private
+#   def destroy
+#     @item.destroy
+#     redirect_to root_path
+#     flash[:alert] = '削除しました。'
+#   end
 
-  def set_item
-    @item = Item.find(params[:id])
-  end
+#   def confirm
+#     if current_user.blank?
+#       redirect_to root_path
+#       flash[:alert] = 'ログインを行なってください。'
+#     else
+#       @card = current_user.cards.first
+#       if @card.present?
+#         customer = Payjp::Customer.retrieve(@card.customer_id)
+#         @default_card_information = customer.cards.retrieve(@card.card_id)
+#       end
+#       @item = Item.find(params[:id])
+#       if @item.soldout == 1
+#         redirect_to item_path(@item.id)
+#       end
+#     end
+#   end
 
-  def items_params
-    params.require(:item).permit(:nickname,:description,:status,:shipping_charges,:area,:days,:price,:category_id,images_attributes: [:image,:id,:_destroy]).merge(user_id: current_user.id)
-  end
+#   private
 
-  # payjpを使用するためのメソッド
-  def get_payjp_info 
-    if Rails.env == 'development'
-      Payjp.api_key = ENV["PAYJP_ACCESS_KEY"]
-    else
-      Payjp.api_key = Rails.application.credentials.payjp[:PAYJP_ACCESS_KEY]
-    end
-  end
+#   def set_item
+#     @item = Item.find(params[:id])
+#   end
 
-end
+#   def items_params
+#     params.require(:item).permit(:nickname,:description,:status,:shipping_charges,:area,:days,:price,:category_id,images_attributes: [:image,:id,:_destroy]).merge(user_id: current_user.id)
+#   end
+
+#   # payjpを使用するためのメソッド
+#   def get_payjp_info 
+#     if Rails.env == 'development'
+#       Payjp.api_key = ENV["PAYJP_ACCESS_KEY"]
+#     else
+#       Payjp.api_key = Rails.application.credentials.payjp[:PAYJP_ACCESS_KEY]
+#     end
+#   end
+
+# end
