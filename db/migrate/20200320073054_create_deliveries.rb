9b4b0179dd3c74786b5d4bbafc9d378592a73428
Neru-dum <tanten180@gmail.com>
2020-03-20 20:24:35 +0900

    JS編集機能　削除機能追加

--- db/migrate/20200320073054_create_deliveries.rb
+++
@@ -1,9 +0,0 @@
-class CreateDeliveries < ActiveRecord::Migration[5.2]
-  def change
-    create_table :deliveries do |t|
-      t.integer :prefecture_id
-      t.string :city
-      t.timestamps
-    end
-  end
-end
